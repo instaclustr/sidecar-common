9b523c6ee6b5abedcfbbe46fc08a79726abb7a58
Stefan Miklosovic <stefan.miklosovic@instaclustr.com>
2020-09-17 19:39:37 +0200

    moved object mapper bits to commons

--- src/main/java/com/instaclustr/sidecar/http/JerseyHttpServerModule.java
+++ src/main/java/com/instaclustr/sidecar/http/JerseyHttpServerModule.java
@@ -4,11 +4,7 @@ import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 import java.net.InetSocketAddress;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.util.concurrent.Service;
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
@@ -17,7 +13,6 @@ import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.multibindings.ProvidesIntoSet;
 import com.instaclustr.guice.GuiceInjectorHolder;
-import com.instaclustr.sidecar.jackson.GuiceJacksonHandlerInstantiator;
 import com.instaclustr.sidecar.jersey.DefaultExceptionMapperProvider;
 import com.instaclustr.sidecar.validation.ValidationConfigurationContextResolver;
 import org.glassfish.hk2.api.ServiceLocator;
@@ -60,25 +55,6 @@ public class JerseyHttpServerModule extends AbstractModule {
                 .register(DefaultExceptionMapperProvider.class)
                 .register(ValidationConfigurationContextResolver.class)
                 .property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
-    }
-
-    @Singleton
-    @Provides
-    ObjectMapper provideObjectMapper(final GuiceJacksonHandlerInstantiator guiceJacksonHandlerInstantiator) {
-
-        final ObjectMapper objectMapper = new ObjectMapper();
-
-        objectMapper.setHandlerInstantiator(guiceJacksonHandlerInstantiator);
-
-        objectMapper.setDefaultPropertyInclusion(JsonInclude.Include.NON_ABSENT);
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        objectMapper.disable(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS);
-
-        objectMapper.registerModule(new Jdk8Module());
-        objectMapper.registerModule(new JavaTimeModule());
-
-        return objectMapper;
     }
 
     /**
